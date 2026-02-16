263dbdcc77a8b1cb5f134c403c64351397977591
Luis <157418831+ldbasilio@users.noreply.github.com>
2026-02-16 21:01:22 +0000

    2/16

--- math_utils.cpp
+++ math_utils.cpp
@@ -1,9 +1,11 @@
 #include "math_utils.h"
  
-int add(int a, int b) {
+int add(int a, int b) 
+{
     return a + b;
 }
  
-int multiply(int a, int b) {
+int multiply(int a, int b) 
+{
     return a * b;
 }
